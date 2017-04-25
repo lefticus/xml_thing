8a696a042b1f354c597fda3253f7de8085c049da
Jason Turner <jason@emptycrate.com>
2017-04-25 16:57:25 -0700

    Try a split out group of pairs

--- xml_parser.cpp
+++ xml_parser.cpp
@@ -10,24 +10,12 @@ struct DOMObject
   DOMObject() = default;
   DOMObject(std::string t_name, std::map<std::string, std::string> t_attrs)
     : name(std::move(t_name)), attributes(std::move(t_attrs))
-  {
-  }
+  { }
   std::string name;
   std::map<std::string, std::string> attributes;
   using DOMElement = std::variant<DOMObject, std::string>;
   std::vector<DOMElement> children;
 };
-
-template<typename T>
-void dump_match(const T &matches) {
-  std::cout << "Match: " << matches.size() << '\n';
-
-  for (size_t i = 0; i < matches.size(); ++i) {
-    auto sub_match = matches[i];
-    std::cout << "  " << i << ':' << sub_match.str() << '\n';
-  }
-}
-
 
 template<typename Char>
 std::map<std::string, std::string> parse_attributes(const std::sub_match<Char> &chars)
@@ -40,67 +28,49 @@ std::map<std::string, std::string> parse_attributes(const std::sub_match<Char> &
       std::regex_iterator<Char>{},
       std::inserter(retval, retval.end()),
       [](const auto &match){ return std::pair{match.str(1), match.str(3)}; }
-      );
+    );
 
   return retval;
 }
 
-
 void parse(std::string_view chars, DOMObject &parent)
 {
-  static const auto regexes = std::tuple{
-    std::pair{
-      std::regex{R"(^<(\S+)(\s.*?)?>((.|\s)*?)<\/\1>)"},
-      [](const auto &match, auto &parent){
-        // node with body
-        parse(
-            std::string_view(match[3].first, match[3].length()),
-            std::get<DOMObject>(parent.children.emplace_back(DOMObject(match.str(1), parse_attributes(match[2]))))
-            );
-      }
-    },
-    std::pair{
-      std::regex{R"(^<(\S+)(\s.*?)?\/>)"},
-      [](const auto &match, auto &parent){
-        // empty node
-        parent.children.emplace_back(DOMObject(match.str(1), parse_attributes(match[2])));
-      }
-    },
-    std::pair{
-      std::regex{R"(^[^<]+)"},
-      [](const auto &match, auto &parent){
-        // cdata
-        parent.children.emplace_back(match.str(0));
-      }
-    },
-    std::pair{
-      std::regex{R"(^\s+)"},
-      [](const auto &, auto &){
-        // whitespace
-      }
+  static const std::pair node_with_body {
+    std::regex{R"(^<(\S+)(\s.*?)?>((.|\s)*?)<\/\1>)"},
+    [](const auto &match, auto &parent){
+      parse( std::string_view(match[3].first, match[3].length()),
+             std::get<DOMObject>(parent.children.emplace_back(DOMObject(match.str(1), parse_attributes(match[2]))))
+          );
     }
   };
 
-  auto find_match = [](const auto &regex, auto &chars, const auto &action, auto &parent){
+  static const std::pair empty_node {
+    std::regex{R"(^<(\S+)(\s.*?)?\/>)"},
+    [](const auto &match, auto &parent){
+      parent.children.emplace_back(DOMObject(match.str(1), parse_attributes(match[2])));
+    }
+  };
+
+  static const std::pair cdata {
+    std::regex{R"(^[^<]+)"}, [](const auto &match, auto &parent){ parent.children.emplace_back(match.str(0)); }
+  };
+
+  static const std::pair whitespace {std::regex{R"(^\s+)"}, [](auto && ...){ } };
+
+  auto find_match = [](const auto &parser, auto &chars, auto &parent){
     if (std::cmatch results;
-        std::regex_search(chars.begin(), chars.end(), results, regex))
+        std::regex_search(chars.begin(), chars.end(), results, parser.first))
     {
       chars.remove_prefix(results.length(0));
-      action(results, parent);
-      return true;
-    } else {
-      return false;
+      parser.second(results, parent);
     }
   };
 
-
   while (!chars.empty()) {
-    std::apply(
-        [&](auto && ... param){
-          (find_match(std::get<0>(param), chars, std::get<1>(param), parent) || ...);
-        },
-        regexes
-      );
+    find_match(node_with_body, chars, parent);
+    find_match(empty_node, chars, parent);
+    find_match(cdata, chars, parent);
+    find_match(whitespace, chars, parent);
   }
 }
 
