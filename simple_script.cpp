7119690b05a1fd29cc9ca7d4891778646dff2f23
Jason Turner <jason@emptycrate.com>
2017-05-08 07:39:04 -0700

    Deduction guides for all function types working

--- simple_script.cpp
+++ simple_script.cpp
@@ -92,88 +92,130 @@ struct [[nodiscard]] Callable_Results : std::pair<bool, Any>
   bool succeeded() const noexcept { return first; }
 };
 
-
-template<typename Ret, typename ... Param>
-struct Function_Signature {
-  template<typename T>
-  Function_Signature(T &&) {}
+template<typename ... Param>
+struct Function_Params
+{
 };
 
-template<typename Ret, typename ... Param>
-Function_Signature(Ret (*f)(Param...)) -> Function_Signature<Ret, Param...>;
+template<typename Ret, typename Params, bool IsObject = false>
+struct Function_Signature {
+  using Param_Types = Params;
+  using Return_Type = Ret;
+  constexpr static const bool is_object = IsObject;
+  template<typename T>
+  Function_Signature(T &&) {}
+  Function_Signature() {}
+};
+
+// Free functions
 
 template<typename Ret, typename ... Param>
-Function_Signature(Ret (*f)(Param...) noexcept) -> Function_Signature<Ret, Param...>;
+Function_Signature(Ret (*f)(Param...)) -> Function_Signature<Ret, Function_Params<Param...>>;
 
+template<typename Ret, typename ... Param>
+Function_Signature(Ret (*f)(Param...) noexcept) -> Function_Signature<Ret, Function_Params<Param...>>;
 
-template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) volatile &) -> Function_Signature<Ret, volatile Class &, Param...>;
-
-template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) volatile & noexcept) -> Function_Signature<Ret, volatile Class &, Param...>;
+// no reference specifier
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) volatile const &) -> Function_Signature<Ret, volatile const Class &, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) volatile) -> Function_Signature<Ret, Function_Params<volatile Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) volatile const & noexcept) -> Function_Signature<Ret, volatile const Class &, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) volatile noexcept) -> Function_Signature<Ret, Function_Params<volatile Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) & ) -> Function_Signature<Ret, Class &, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) volatile const) -> Function_Signature<Ret, Function_Params<volatile const Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) & noexcept) -> Function_Signature<Ret, Class &, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) volatile const noexcept) -> Function_Signature<Ret, Function_Params<volatile const Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) const &) -> Function_Signature<Ret, const Class &, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) ) -> Function_Signature<Ret, Function_Params<Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) const & noexcept) -> Function_Signature<Ret, const Class &, Param...>;
-
-
-
+Function_Signature(Ret (Class::*f)(Param ...) noexcept) -> Function_Signature<Ret, Function_Params<Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) volatile &&) -> Function_Signature<Ret, volatile Class &&, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) const) -> Function_Signature<Ret, Function_Params<const Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) volatile && noexcept) -> Function_Signature<Ret, volatile Class &&, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) const noexcept) -> Function_Signature<Ret, Function_Params<const Class &, Param...>>;
+
+// & reference specifier
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) volatile const &&) -> Function_Signature<Ret, volatile const Class &&, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) volatile &) -> Function_Signature<Ret, Function_Params<volatile Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) volatile const && noexcept) -> Function_Signature<Ret, volatile const Class &&, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) volatile & noexcept) -> Function_Signature<Ret, Function_Params<volatile Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) &&) -> Function_Signature<Ret, Class &&, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) volatile const &) -> Function_Signature<Ret, Function_Params<volatile const Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) && noexcept) -> Function_Signature<Ret, Class &&, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) volatile const & noexcept) -> Function_Signature<Ret, Function_Params<volatile const Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) const &&) -> Function_Signature<Ret, const Class &&, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) & ) -> Function_Signature<Ret, Function_Params<Class &, Param...>>;
 
 template<typename Ret, typename Class, typename ... Param>
-Function_Signature(Ret (Class::*f)(Param ...) const && noexcept) -> Function_Signature<Ret, const Class &&, Param...>;
+Function_Signature(Ret (Class::*f)(Param ...) & noexcept) -> Function_Signature<Ret, Function_Params<Class &, Param...>>;
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) const &) -> Function_Signature<Ret, Function_Params<const Class &, Param...>>;
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) const & noexcept) -> Function_Signature<Ret, Function_Params<const Class &, Param...>>;
+
+// && reference specifier
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) volatile &&) -> Function_Signature<Ret, Function_Params<volatile Class &&, Param...>>;
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) volatile && noexcept) -> Function_Signature<Ret, Function_Params<volatile Class &&, Param...>>;
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) volatile const &&) -> Function_Signature<Ret, Function_Params<volatile const Class &&, Param...>>;
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) volatile const && noexcept) -> Function_Signature<Ret, Function_Params<volatile const Class &&, Param...>>;
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) &&) -> Function_Signature<Ret, Function_Params<Class &&, Param...>>;
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) && noexcept) -> Function_Signature<Ret, Function_Params<Class &&, Param...>>;
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) const &&) -> Function_Signature<Ret, Function_Params<const Class &&, Param...>>;
+
+template<typename Ret, typename Class, typename ... Param>
+Function_Signature(Ret (Class::*f)(Param ...) const && noexcept) -> Function_Signature<Ret, Function_Params<const Class &&, Param...>>;
 
 
 template<typename Func>
-Function_Signature(Func &&) -> Function_Signature<decltype(Function_Signature{&std::decay_t<Func>::operator()})>;
+Function_Signature(Func &&) -> Function_Signature<
+    typename decltype(Function_Signature{&std::decay_t<Func>::operator()})::Return_Type,
+    typename decltype(Function_Signature{&std::decay_t<Func>::operator()})::Param_Types,
+    true
+  >;
 
   
 struct GenericCallable
 {
   template<typename Func, typename ... Param, size_t ... Indexes>
-    static auto my_invoke(Func &f, Array_View<Any> params, std::index_sequence<Indexes...>)
+    static decltype(auto) my_invoke(Func &f, Array_View<Any> params, std::index_sequence<Indexes...>)
     {
-      return std::invoke(std::forward<Func>(f), cast<Param>(params[Indexes])...);
+      return (std::invoke(std::forward<Func>(f), cast<Param>(params[Indexes])...));
     };
 
+  struct Tag
+  {
+  };
 
-
-  template<typename Func, typename Ret, typename ... Param>
-  GenericCallable(Func &&func, Function_Signature<Ret, Param...>)
+  template<typename Func, bool Is_Object, typename Ret, typename ... Param>
+  GenericCallable(Func &&func, Function_Signature<Ret, Function_Params<Param...>, Is_Object>, Tag)
     : caller (
         [func = std::forward<Func>(func)](Array_View<Any> params) mutable {
         return std::pair<bool, Any>(
@@ -183,6 +225,16 @@ struct GenericCallable
         }
         )
     {}
+
+  template<typename Func, typename Ret, typename Object, typename ... Param>
+  GenericCallable(Func &&func, Function_Signature<Ret, Function_Params<Object, Param...>, true>)
+    : GenericCallable(std::forward<Func>(func), Function_Signature<Ret, Function_Params<Param...>, true>{}, Tag{})
+  {}
+
+  template<typename Func, typename Ret, typename ... Param>
+  GenericCallable(Func &&func, Function_Signature<Ret, Function_Params<Param...>, false>)
+    : GenericCallable(std::forward<Func>(func), Function_Signature<Ret, Function_Params<Param...>, false>{}, Tag{})
+  {}
 
   template<typename Func, typename = std::enable_if_t<!std::is_same_v<std::decay_t<Func>, GenericCallable>>>
   GenericCallable(Func &&f) 
@@ -213,7 +265,7 @@ struct Int
     return m_val + j;
   }
 
-  int add(double k) const volatile noexcept {
+  int add2(double k) const volatile noexcept {
     return m_val * 2 + k;
   }
 
@@ -230,21 +282,28 @@ struct Simple_Script
   std::map<std::string, Dispatcher> dispatchers;
 };
 
+
+
 int main()
 {
   Simple_Script script;
-  script.dispatchers["func"].emplace_back(&my_func);
-  script.dispatchers["add"].emplace_back(&Int::add);
-  script.dispatchers["add"].emplace_back(&Int::add2);
-  script.dispatchers["lambda"].emplace_back(&my_func);
+  script.dispatchers["func"].callables.emplace_back(&my_func);
+  script.dispatchers["add"].callables.emplace_back(&Int::add);
+  script.dispatchers["add"].callables.emplace_back(&Int::add2);
+  script.dispatchers["lambda"].callables.emplace_back(&my_func);
+  script.dispatchers["add"].callables.emplace_back([](double d, double d2) { return d + d2; });
 
-  std::array<Any, 2> params{1,2};
-  std::cout << cast<int>(callable.caller(Array_View{params}).second) << '\n';
+  GenericCallable callable{[](double d, double d2) { return d + d2; }};
 
-  std::array<Any, 2> params2{Int(2), 4};
-  std::cout << cast<int>(callable2.caller(Array_View{params2}).second) << '\n';
+  //return std::invoke([](double d, double d2) { return d + d2;}, 5.2, 6.4);
 
-  std::cout << cast<int>(callable4.caller(Array_View{params2}).second) << '\n';
+//  std::array<Any, 2> params{1,2};
+//  std::cout << cast<int>(callable.caller(Array_View{params}).second) << '\n';
+
+//  std::array<Any, 2> params2{Int(2), 4};
+//  std::cout << cast<int>(callable2.caller(Array_View{params2}).second) << '\n';
+
+//  std::cout << cast<int>(callable4.caller(Array_View{params2}).second) << '\n';
 }
 
 
