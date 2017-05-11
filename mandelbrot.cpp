88621b5a751e2d97d822e91730f55e3e0c39e454
Jason Turner <jason@emptycrate.com>
2017-05-11 16:29:20 -0600

    Allow changing of power,abs

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <thread>
 #include <future>
+#include <complex>
+#include <cassert>
 
 template<typename T>
 struct Point {
@@ -85,30 +87,67 @@ struct Color {
 template<typename T>
 Color(T r, T g, T b) -> Color<T>;
 
-template<typename PointType, typename CenterType, typename SizeType>
-auto get_color(const Point<PointType> &t_point, const Point<CenterType> &t_center, 
-               const Size<SizeType> &t_size, const double t_scale) noexcept
+template<std::size_t Power, typename Value>
+constexpr auto pow(Value t_val)
 {
-  auto xscaled = t_point.x/(t_size.width/t_scale) + (t_center.x - (t_scale/2.0 ));
-  auto yscaled = t_point.y/(t_size.height/t_scale) + (t_center.y - (t_scale/2.0 ));
+  auto result = t_val;
+  for(std::size_t itr = 1; itr < Power; ++itr)
+  {
+    result *= t_val;
+  }
+  return result;
+}
 
-  auto x = xscaled;
-  auto y = yscaled;
+template<typename ComplexType, typename PowerType>
+constexpr auto opt_pow(const std::complex<ComplexType> &t_val, PowerType t_power)
+{
+  if (t_power == static_cast<PowerType>(1.0)) {
+    return t_val;
+  } else if (t_power == static_cast<PowerType>(2.0)) {
+    return std::complex{ pow<2>(std::real(t_val)) - pow<2>(std::imag(t_val)), 2 * std::real(t_val) * std::imag(t_val) };
+  } else if (t_power == static_cast<decltype(t_power)>(3.0)) {
+    const auto a = std::real(t_val);
+    const auto b = std::imag(t_val);
+    return std::complex{ -3 * a * pow<2>(b) + pow<3>(a), 3 * pow<2>(a) * b - pow<3>(b) };
+//  } else if (t_power == static_cast<decltype(t_power)>(4.0)) {
+//    const auto a = std::real(t_val);
+//    const auto b = std::imag(t_val);
+//    return std::complex{ pow<4>(a) + pow<4>(b) - 6 * pow<2>(a) * pow<2>(b), 4 * pow<3>(a) * b - 4 * a * pow<3>(b) };
+  } else {
+    return std::pow(t_val, t_power);
+  }
 
-  auto iteration = 0;
-  auto max_iteration = 2000;
+}
 
+template<typename PointType, typename CenterType, typename SizeType, typename ScaleType>
+auto get_color(const Point<PointType> &t_point, const Point<CenterType> &t_center, 
+               const Size<SizeType> &t_size, const ScaleType t_scale, std::size_t max_iteration,
+               const CenterType power, const bool do_abs) noexcept
+{
+  const std::complex scaled {
+    t_point.x/(t_size.width/t_scale) + (t_center.x - (t_scale/static_cast<CenterType>(2.0) )),
+    t_point.y/(t_size.height/t_scale) + (t_center.y - (t_scale/static_cast<CenterType>(2.0) ))
+  };
+
+  auto current = scaled;
+
+  auto iteration = 0u;
   auto stop_iteration = max_iteration;
 
   while ( iteration < stop_iteration )
   {
-    if ((x*x) + (y*y) > (2.0*2.0) && stop_iteration == max_iteration) {
+    if (std::norm(current) > (2.0*2.0) && stop_iteration == max_iteration) {
       stop_iteration = iteration + 5;
     }
 
-    auto xtemp = (x*x) - (y*y) + xscaled;
-    y = (2.0*x*y) + yscaled;
-    x = xtemp;
+    if (do_abs) {
+      current = std::complex{ std::abs(std::real(current)), std::abs(std::imag(current)) };
+    }
+
+    current = opt_pow(current, power);
+    current += scaled;
+
+
     ++iteration;
   }
 
@@ -116,8 +155,8 @@ auto get_color(const Point<PointType> &t_point, const Point<CenterType> &t_cente
   {
     return Color{0.0,0.0,0.0};
   } else {
-    const auto value = ((iteration + 1) - (std::log(std::log(std::abs(x * y))))/std::log(2.0)); 
-    const auto colorval = static_cast<int>(std::floor(value * 10.0));
+    const auto value = ((iteration + 1) - (std::log(std::log(std::abs(std::real(current) * std::imag(current)))))/std::log(power)); 
+    const auto colorval = std::abs(static_cast<int>(std::floor(value * 10.0)));
 
     const auto colorband = colorval % (256 * 7) / 256;
     const auto mod256 = colorval % 256;
@@ -139,6 +178,9 @@ auto get_color(const Point<PointType> &t_point, const Point<CenterType> &t_cente
         return Color{to_1, 0.0, 1.0};
       case 6:
         return Color{to_0, 0.0, to_0};
+      default:
+        std::cout << "colorband: " << colorband << '\n';
+        assert(colorband <= 6 && colorband >= 0);
     }
   }
 }
@@ -161,7 +203,6 @@ struct SetPixel {
       };
       return sf::Color(to_8bit(color.r), to_8bit(color.g), to_8bit(color.b));
     };
-    const auto color = to_sf_color(t_color);
     img.setPixel(t_point.x, t_point.y, to_sf_color(t_color));
   }
 };
@@ -182,21 +223,21 @@ struct ImageBlock
   static constexpr const auto size = Size<std::size_t>{BlockWidth, BlockHeight};
 };
 
-template<size_t BlockWidth, size_t BlockHeight, typename SizeType, typename CenterType, typename Container>
+template<size_t BlockWidth, size_t BlockHeight, typename SizeType, typename CenterType, typename ScaleType, typename Container>
 void future_pixels(const Size<SizeType> &t_size, const Point<CenterType> &t_center,
-                   const double t_scale, Container &t_container)
+                   const ScaleType t_scale, Container &t_container, size_t t_max_iteration, const CenterType t_power, const bool t_do_abs)
 {
   t_container.clear();
 
   for (const auto [x, y] : t_size.iterable(BlockWidth, BlockHeight)) {
     t_container.push_back(
         std::async(
-          [p = Point{x, y}, t_size, t_center, t_scale](){
+          [p = Point{x, y}, t_size, t_center, t_scale, t_max_iteration, t_power, t_do_abs](){
             using Row = std::array<Color<double>, BlockWidth>;
             ImageBlock<BlockWidth, BlockHeight> image{p};
 
             for (const auto [x, y] : image.size.iterable(1,1)) {
-              image.image[y][x] = get_color(Point{p.x + x, p.y + y}, t_center, t_size, t_scale);
+              image.image[y][x] = get_color(Point{p.x + x, p.y + y}, t_center, t_size, t_scale, t_max_iteration, t_power, t_do_abs);
             }
             return image;
           }
@@ -206,7 +247,7 @@ void future_pixels(const Size<SizeType> &t_size, const Point<CenterType> &t_cent
 }
 
 template<typename Container>
-bool cull_pixels(Container &t_container, SetPixel &t_set_pixel)
+std::pair<bool, bool> cull_pixels(Container &t_container, SetPixel &t_set_pixel)
 {
   const auto done = std::remove_if(t_container.begin(), t_container.end(), 
       [&](auto &f) {
@@ -231,7 +272,7 @@ bool cull_pixels(Container &t_container, SetPixel &t_set_pixel)
 
   t_container.erase(done, t_container.end());
 
-  return did_something;
+  return std::pair{did_something, t_container.empty()};
 }
 
 int main() {
@@ -252,19 +293,31 @@ int main() {
   bufferSprite.setTextureRect(sf::IntRect(0,0,size.width,size.height));
   bufferSprite.setPosition(0,0);
 
-  Point center{0.001643721971153l, -0.822467633298876l};
-  auto scale = 0.01;
+  Point center{0.001643721971153, -0.822467633298876};
+  auto scale = 3.0;
+  auto power = 2.0;
+  auto do_abs = false;
 
   std::vector<std::future<ImageBlock<block_size.width, block_size.height>>> pixels;
 
-  auto see_the_future = [&](){
-    future_pixels<block_size.width, block_size.height>(size, center, scale, pixels);
+  auto see_the_future = [&](const size_t t_max_iterations, const auto t_power, const bool t_do_abs){
+    std::cout<<" New Future: (" << center.x << ", " << center.y << ") iters: " << t_max_iterations << " scale: " << scale << " power: " << t_power << " do_abs: " << t_do_abs << '\n';
+    future_pixels<block_size.width, block_size.height>(size, center, scale, pixels, t_max_iterations, t_power, t_do_abs);
   };
 
-  see_the_future();
+
+  const std::size_t max_max_iterations = 2000;
+
+  const std::size_t max_iteration_increment = 200;
+  const std::size_t start_max_iterations = 400;
+
+  std::size_t cur_max_iterations = start_max_iterations;
+
+  see_the_future(cur_max_iterations, power, do_abs);
 
   while (window.isOpen()) {
-    if (cull_pixels(pixels, sp)) {
+    const auto [culled_pixels, none_left] = cull_pixels(pixels, sp);
+    if (culled_pixels) {
       texture.loadFromImage(img);
     }
 
@@ -303,12 +356,34 @@ int main() {
           center.y += move_offset;
           changed = true;
         }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::P)) {
+          if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
+            power += 0.1;
+          } else {
+            power -= 0.1;
+          }
+          changed = true;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+          do_abs = !do_abs;
+          changed = true;
+        }
+
+
 
         return changed;
       }();
 
     if (future_changed) {
-      see_the_future();
+      cur_max_iterations = start_max_iterations;
+      see_the_future(cur_max_iterations, power, do_abs);
+    } else if (none_left) {
+      if (cur_max_iterations < max_max_iterations) {
+        cur_max_iterations += max_iteration_increment;
+        see_the_future(cur_max_iterations, power, do_abs);
+      } else {
+        std::this_thread::yield();
+      }
     }
   }
 }
